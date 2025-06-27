55723aaffa336dda7f32267d1844a6391086e5ec
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-06-27 15:36:39 +0530

    about sec

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,40 @@ export default function PortFolio() {
             </div>
           </div>
         </div>
+
+        <div className="about mt-10">
+          <p className="font-semibold text-xl">About Me</p>
+          <p className="mt-2">
+            20, reading fuels me - but building grounds me.
+          </p>
+          <p>
+            I just write code, break it, learn fast, make it happen, repeat.
+          </p>
+          <p>
+            Deeply intrested in Cricket, history, Video Editing and business
+            insights as well.
+          </p>
+          <span className="flex gap-4 mt-5">
+            <button className="relative px-6 py-2 rounded-xl text-white font-semibold border border-transparent overflow-hidden cursor-pointer group z-10 bg-black">
+              <span className="absolute inset-0 rounded-xl border-[3px] border-white hover:border-transparent transition-all duration-700">
+                <span className="absolute inset-0 animate-spin rounded-xl border-[3px] border-transparent border-t-pink-500 border-l-yellow-400 border-b-green-400 border-r-blue-500 blur-sm"></span>
+              </span>
+
+              <span className="absolute inset-0 bg-white opacity-0 group-hover:opacity-10 transition-opacity duration-700 rounded-xl"></span>
+
+              <span className="relative z-10">Book a meet</span>
+            </button>
+            <button className="relative px-6 py-2 rounded-xl text-white font-semibold border border-transparent overflow-hidden cursor-pointer group z-10 bg-black">
+              <span className="absolute inset-0 rounded-xl border-[3px] border-white hover:border-transparent transition-all duration-700">
+                <span className="absolute inset-0 animate-spin rounded-xl border-[3px] border-transparent border-t-pink-500 border-l-yellow-400 border-b-green-400 border-r-blue-500 blur-sm"></span>
+              </span>
+
+              <span className="absolute inset-0 bg-white opacity-0 group-hover:opacity-10 transition-opacity duration-700 rounded-xl"></span>
+
+              <span className="relative z-10">Get in Touch</span>
+            </button>
+          </span>
+        </div>
       </div>
     </div>
   );
--- components/icons/icons.tsx
+++ components/icons/icons.tsx
@@ -72,3 +72,19 @@ export function Mail() {
     </div>
   );
 }
+
+export function AnimatePulse() {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M12 18a6 6 0 1 0 0-12 6 6 0 0 0 0 12Z"></path>
+    </svg>
+  );
+}
