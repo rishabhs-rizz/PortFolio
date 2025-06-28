d0e7eb9b3f93f436779a131619f39956bc6d2911
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-06-28 12:34:01 +0530

    added shiny buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Twitter, LinkedIn, Github, Mail } from "@/components/icons/icons";
-
+import ShinyButton from "@/components/buttons/button";
 export default function PortFolio() {
   return (
     <div className="min-h-screen bg-black text-white flex justify-center">
@@ -24,7 +24,6 @@ export default function PortFolio() {
             </div>
           </div>
         </div>
-
         <div className="about mt-10">
           <p className="font-semibold text-xl">About Me</p>
           <p className="mt-2">
@@ -37,17 +36,10 @@ export default function PortFolio() {
             Deeply intrested in Cricket, history, Video Editing and business
             insights as well.
           </p>
-          <span className="flex gap-4 mt-5">
-            <button className="relative px-6 py-2 rounded-xl text-white font-semibold border border-transparent overflow-hidden cursor-pointer group z-10 bg-black">
-              <span className="absolute inset-0 rounded-xl border-[3px] border-white hover:border-transparent transition-all duration-700">
-                <span className="absolute inset-0 animate-spin rounded-xl border-[3px] border-transparent border-t-pink-500 border-l-yellow-400 border-b-green-400 border-r-blue-500 blur-sm"></span>
-              </span>
-
-              <span className="absolute inset-0 bg-white opacity-0 group-hover:opacity-10 transition-opacity duration-700 rounded-xl"></span>
-
-              <span className="relative z-10">Book a meet</span>
-            </button>
-            <button className="relative px-6 py-2 rounded-xl text-white font-semibold border border-transparent overflow-hidden cursor-pointer group z-10 bg-black">
+          <span className="buttons flex gap-4 mt-5">
+            <ShinyButton text="Book a Call" />
+            <ShinyButton text={"Get in Touch"} />
+            {/* <button className="relative px-6 py-2 rounded-xl text-white font-semibold border border-transparent overflow-hidden cursor-pointer group z-10 bg-black">
               <span className="absolute inset-0 rounded-xl border-[3px] border-white hover:border-transparent transition-all duration-700">
                 <span className="absolute inset-0 animate-spin rounded-xl border-[3px] border-transparent border-t-pink-500 border-l-yellow-400 border-b-green-400 border-r-blue-500 blur-sm"></span>
               </span>
@@ -55,7 +47,7 @@ export default function PortFolio() {
               <span className="absolute inset-0 bg-white opacity-0 group-hover:opacity-10 transition-opacity duration-700 rounded-xl"></span>
 
               <span className="relative z-10">Get in Touch</span>
-            </button>
+            </button> */}
           </span>
         </div>
       </div>
